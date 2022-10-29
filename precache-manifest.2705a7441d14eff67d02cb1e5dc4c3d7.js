fe348d6db85f807cb4ee2b223bfa017bb1a380bf
Priyansh Salian <79307288+priyanshpsalian@users.noreply.github.com>
2022-10-29 23:16:22 +0530

    Updates

---
+++ precache-manifest.2705a7441d14eff67d02cb1e5dc4c3d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9fb4f9a4fa6bdbe3b1889796cf29d0",
+    "url": "/Image-Search-Application/index.html"
+  },
+  {
+    "revision": "d19dc7c2522070b537f3",
+    "url": "/Image-Search-Application/static/css/main.30f1b11e.chunk.css"
+  },
+  {
+    "revision": "08b3c966dab06e4190cd",
+    "url": "/Image-Search-Application/static/js/2.d26f9645.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/Image-Search-Application/static/js/2.d26f9645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19dc7c2522070b537f3",
+    "url": "/Image-Search-Application/static/js/main.c65b1b39.chunk.js"
+  },
+  {
+    "revision": "3a98f116d6bfe537848a",
+    "url": "/Image-Search-Application/static/js/runtime-main.6ebf5045.js"
+  }
+]);
